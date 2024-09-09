a9a64802057c7366c149ea09da8c159a07a88a57
keval <keval@middleware.io>
2024-09-09 15:18:12 +0530

    Allow excluding spans based on service name

--- src/tracer-collector.ts
+++ src/tracer-collector.ts
@@ -9,10 +9,36 @@ const { CompositePropagator } = require('@opentelemetry/core');
 const { B3Propagator, B3InjectEncoding } = require('@opentelemetry/propagator-b3');
 import {Config} from './config';
 const { MongooseInstrumentation } = require('@opentelemetry/instrumentation-mongoose');
+import { SpanExporter, type ReadableSpan, SimpleSpanProcessor, BasicTracerProvider } from '@opentelemetry/sdk-trace-base';
 export const init = (config: Config) => {
     const apm_pause_traces = config.pauseTraces === true;
 
     if (!apm_pause_traces) {
+
+        // Custom Span Processor to filter spans by service name
+        class FilterSpanProcessor extends SimpleSpanProcessor {
+            private excludedServices: string[];
+            constructor(exporter: SpanExporter, excludedServices: string[]) {
+                super(exporter);
+                this.excludedServices = excludedServices;
+            }
+
+            onEnd(span: ReadableSpan) {
+                const serviceName = span.resource.attributes[SEMRESATTRS_SERVICE_NAME] || "";
+                
+                // Only export spans that don't match the excluded service name
+                if (typeof serviceName === "string" && !this.excludedServices.includes(serviceName)) {   
+                    super.onEnd(span);
+                }
+            }
+        }
+
+        const traceExporter = new OTLPTraceExporter({
+            url: config.target,
+        });
+        const excludedServices = process.env.MW_APM_EXCLUDED_SERVICES || "";
+        const excludedServicesArray: string[] = excludedServices.split(',').map(item => item.trim());
+
         const sdk = new opentelemetry.NodeSDK({
             CompositePropagator: new CompositePropagator({
                 propagators: [
@@ -20,9 +46,8 @@ export const init = (config: Config) => {
                     new B3Propagator({ injectEncoding: B3InjectEncoding.MULTI_HEADER }),
                 ],
             }),
-            traceExporter: new OTLPTraceExporter({
-                url: config.target,
-            }),
+            spanProcessor: new FilterSpanProcessor(traceExporter, excludedServicesArray),
+            traceExporter: traceExporter,
             instrumentations: [
                 getNodeAutoInstrumentations({}),
                 new GrpcInstrumentation({
